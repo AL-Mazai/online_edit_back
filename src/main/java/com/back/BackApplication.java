8acf1966e1d82a416df7598a94de4d1bf9ecd48f
zzw <3279527604@qq.com>
2023-05-03 12:55:58 +0800

    测试

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
         SpringApplication.run(BackApplication.class, args);
     }
 
